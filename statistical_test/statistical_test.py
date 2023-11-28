66fe1d3e017594398975499ca07492a432a796be
Lorenzo Bazzanini <99730485+LBasz@users.noreply.github.com>
2023-11-28 12:04:16 +0100

    minor changes to the plot functions

--- statistical_test/statistical_test.py
+++ statistical_test/statistical_test.py
@@ -1341,10 +1341,10 @@ def make_plot(instrument, test_times,
     #--------------------------------------------------------------------------#
 
     # plots
-    ax[0,0].plot(test_times**(1/3),     averaged_fluxes,             color='b', alpha=1.00, label = label_instr)
-    ax[0,0].plot(test_times**(1/3),     averaged_fluxes_sim,         color='r', alpha=0.75, label = r'Simulated')
-    ax[0,0].plot(test_times[1:]**(1/3), averaged_fluxes_rms[1:],     color='b', alpha=1.00)
-    ax[0,0].plot(test_times[1:]**(1/3), averaged_fluxes_rms_sim[1:], color='r', alpha=0.75)
+    ax[0,0].plot(test_times**(1/3),     averaged_fluxes,                color='b', lw=1.5, alpha=1.00, label = label_instr)
+    ax[0,0].plot(test_times**(1/3),     averaged_fluxes_sim,            color='r', lw=1.5, alpha=0.75, label = r'Sim (GA)')
+    ax[0,0].plot(test_times[1:]**(1/3), averaged_fluxes_rms[1:],        color='b', lw=1.5, alpha=1.00)
+    ax[0,0].plot(test_times[1:]**(1/3), averaged_fluxes_rms_sim[1:],    color='r', lw=1.5, alpha=0.75)
     # error bars
     if err_bars:
         errs     = averaged_fluxes_rms     / np.sqrt(n_grb_real)
@@ -1391,8 +1391,8 @@ def make_plot(instrument, test_times,
     #--------------------------------------------------------------------------#
 
     # plots
-    ax[0,1].plot(test_times**(1/3), averaged_fluxes_cube,     color='b', label=label_instr)
-    ax[0,1].plot(test_times**(1/3), averaged_fluxes_cube_sim, color='r', label='Simulated', alpha=0.75)
+    ax[0,1].plot(test_times**(1/3), averaged_fluxes_cube,     color='b', lw=1.5, label=label_instr)
+    ax[0,1].plot(test_times**(1/3), averaged_fluxes_cube_sim, color='r', lw=1.5, label='Sim (GA)',   alpha=0.75)
     # error bars
     if err_bars:
         errs     = averaged_fluxes_cube_rms     / np.sqrt(n_grb_real)
@@ -1437,8 +1437,8 @@ def make_plot(instrument, test_times,
     #--------------------------------------------------------------------------#
 
     # plots
-    ax[1,0].plot((steps    *bin_time)**(1/3), acf,     color='b', label=label_instr)
-    ax[1,0].plot((steps_sim*bin_time)**(1/3), acf_sim, color='r', label='Simulated', alpha=0.75)
+    ax[1,0].plot((steps    *bin_time)**(1/3), acf,     color='b', lw=1.5, label=label_instr)
+    ax[1,0].plot((steps_sim*bin_time)**(1/3), acf_sim, color='r', lw=1.5, label='Sim (GA)', alpha=0.75)
     # error bars
     if err_bars:
         errs     = acf_rms     / np.sqrt(n_grb_real)
@@ -1566,8 +1566,8 @@ def make_plot(instrument, test_times,
         kde_real     = y_plot_real.sum(0)
         kde_sim      = y_plot_sim.sum(0)
         # plot
-        ax[1,1].plot(x_grid, kde_real, c='b', lw=3, label=label_instr, zorder=5)
-        ax[1,1].plot(x_grid, kde_sim,  c='r', lw=3, label='Simulated', zorder=6)
+        ax[1,1].plot(x_grid, kde_real, c='b', lw=1.5, label=label_instr, zorder=5)
+        ax[1,1].plot(x_grid, kde_sim,  c='r', lw=1.5, label='Sim (GA)',  zorder=6)
         # errors
         if err_bars:
             n_resample=500
